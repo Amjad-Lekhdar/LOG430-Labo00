b43b63c174d0f503410b8e880e79fb0756acb993
Amjad-Lekhdar <amjadlekhdar@gmail.com>
2026-05-13 21:33:57 -0400

    Fix calculator tests by instantiating Calculator in each test function

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -11,8 +11,19 @@ def test_app():
     welcome_message = my_calculator.get_hello_message()
     assert "== Calculatrice v1.0 ==" in welcome_message
 
+# Test de l'addition
 def test_addition():
-    assert addition(2, 3) == 5
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
 
+# Test de la soustraction
 def test_subtraction():
-    assert subtraction(5, 3) == 2
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(5, 2) == 3
+
+    
+# Test fautif de la soustraction
+def test_subtraction():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(5, 2) == 4
+
